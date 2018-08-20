6f546170ae27907caee916eeb92c2789ba8ebc91
Chris Weilacker <chrisweilacker@localhost.localdomain>
2018-08-20 14:41:55 -0700

    Completed Bare Minimum Requirements for recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function gEBCN (className, elementNode) {
   // your code here
+  //create return array of elements
+  var returnArr = [];
+  //check if elementNode exists if not start at body
+  //, if body doesnt exist just return a blank array.
+  if (elementNode === undefined) {
+    if(document.body!==null) {
+      elementNode = document.body;
+    } else {
+      return returnArr;
+    }
+  }
+  //get list of classes on the current node.
+  var classListings = elementNode.classList;
+  //check if className exists in the class list
+  // and add the element to the return array if it does
+  if (classListings !== undefined) {
+    for (var i =0; i<classListings.length; i++) {
+      if (classListings.item(i) === className) {
+        returnArr.push(elementNode);
+      }
+    }
+  }
+  
+//check if it has child nodes and recursively call the function
+// on each child node to build out the array to return. 
+  if (elementNode.hasChildNodes()) {
+    var children = elementNode.childNodes;
+  
+    for (var i = 0; i < children.length; i++) {
+      //
+      returnArr = returnArr.concat(gEBCN(className, children[i]));
+    }
+  }
+
+  return returnArr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //check the type of passed in obj and handle it according to the rules of JSON.stringify
+  if (typeof(obj) === "string") {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    //if is array create an array string 
+    //and go through the indexes and build the string by recursively calling the function.
+    var returnStr = "[";
+
+    for (var i =0; i<obj.length; i++) {
+        returnStr += stringifyJSON(obj[i]);
+        if (i!==obj.length-1) {
+          returnStr+=",";
+        }
+    }
+    returnStr += "]";
+    return returnStr;
+  } else if (typeof(obj) === "boolean" || typeof(obj) === "number") {
+    //if boolean or number use the .toString method
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof(obj) === "function" || typeof(obj) === "symbol" || obj === undefined) {
+    //if undefined a symbol or function return an empty string.
+    return "";
+  } else if (typeof(obj) === "object") {
+    //create the return str with curly braces for an object.
+    var returnStr = "{";
+    for (var key in obj) {
+      let stringifyValue = stringifyJSON(obj[key]);
+      //if empty dont list that key
+      if (stringifyValue!==null && stringifyValue!==undefined && stringifyValue!=="") {
+        if (returnStr[returnStr.length-1]!== "{") {
+          returnStr+=",";
+        }
+        returnStr += '"' + key + '":' + stringifyValue;
+      }
+    }
+      returnStr += "}";    
+    return returnStr;
+  } else {
+    //catch all return empty string should never reach but just in case
+    return '';
+  }
 };
